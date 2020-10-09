5426bfbeccbe2b698f903763be916d5aa7d4eb83
Pavel Yakovlev <pavel.yakovlev.1984@bk.ru>
2020-10-09 20:07:21 +0300

    Первый коммит

---
+++ lesson01/script/script.js
@@ -0,0 +1,12 @@
+// alert('Hello friends');
+
+let money = 10;
+let income = 5;
+let addExpenses = 9;
+let deposit = 3;
+let mission = 1;
+let period = "dfdfdf";
+
+alert('dfdf');
+
+console.log('5555');
