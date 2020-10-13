8d1c4feed82c262bc77d4fcfe4a7a08bc8749b66
Pavel Yakovlev <pavel.yakovlev.1984@bk.ru>
2020-10-13 13:40:02 +0300

    lesson02

---
+++ lesson02/js/script.js
@@ -0,0 +1,52 @@
+// myVar={};
+// myArr=[];
+// let myTrue = true;
+// let myFalse = false;
+
+// console.log(2===2)
+// console.log(2===3)
+
+// console.log(typeof myArr);
+// console.log(Boolean(5));
+// console.log(Boolean("Hello"));
+// let i=5;
+// console.log(9%2)
+// console.log(i)
+// let myString = 'apple, kiwi, orange';
+// // console.log(myString.toUpperCase());
+// // console.log(myString.toLowerCase());
+// // console.log(myString[0]);
+// console.log(myString.split(',  '));
+
+let money =45000;
+
+let income =15000;
+
+ let addExpenses = 'Rent, Meal, Fuel';  
+ let deposit = 40000;
+ let mission = 6000000;
+ let period =6000;
+  console.log(money);
+  console.log(income); 
+  console.log(deposit); 
+
+  console.log(typeof money);
+  console.log(typeof income);
+  console.log(typeof deposit);
+
+
+  console.log(addExpenses.length);
+
+
+  console.log('"Период равен'+' '+ period +' '+'месяцев” и “Цель заработать' +' '+ mission + ' '+'рублей"');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(' '));
+
+
+console.log(budgetDay=45000/30);
+
+
+
+
+
