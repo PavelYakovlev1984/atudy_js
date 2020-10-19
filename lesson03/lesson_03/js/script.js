3a00e4dcf0dc6689d720d78a33b16318dc079425
Pavel Yakovlev <pavel.yakovlev.1984@bk.ru>
2020-10-19 14:52:11 +0300

    lesson_3_o1

---
+++ lesson03/lesson_03/js/script.js
@@ -0,0 +1,85 @@
+
+let money =45000;
+
+let income = 'freelance';
+
+ let addExpenses = 'Rent, Meal, Fuel';  
+ let deposit = false;
+ let mission = 600000;
+ let period =12;
+  console.log(money);
+  console.log(income); 
+  console.log(deposit); 
+
+  console.log(typeof money);
+  console.log(typeof income);
+  console.log(typeof deposit);
+
+
+  console.log(addExpenses.length);
+
+
+  console.log('"Период равен'+' '+ period +' '+'месяцев” и “Цель заработать' +' '+ mission + ' '+'рублей"');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(' '));
+
+
+ let budgetDay = money / 30;
+console.log(budgetDay);
+
+
+ money = prompt('“Ваш месячный доход?”');
+
+
+ addExpenses = prompt('“Перечислите возможные расходы за рассчитываемый период через запятую”');
+
+ deposit = Boolean(prompt('“Есть ли у вас депозит в банке?”'));
+
+ expenses1 = prompt('“Введите обязательную статью расходов?”');
+ expenses2 = prompt('“Введите обязательную статью расходов?”');
+ amount1 = prompt('“Во сколько это обойдется?”');
+ damount2 = prompt('“Во сколько это обойдется?”');
+
+
+ 
+ 
+ let budgetMonth = money - addExpenses;
+
+console.log('Бюджет на месяц'+ budgetMonth);
+
+let missionPeriod = mission / budgetMonth;
+
+console.log('Бюджет на месяц'+ missionPeriod);
+
+
+let MPeriod = Math.ceil(missionPeriod);
+
+console.log('Цель будет достигнута за:'+ missionPeriod + 'месяца');
+
+
+
+budgetDay = budgetMonth / 30;
+
+console.log('Бюджет на день:'+ budgetDay);
+
+let n = budgetDay;
+
+if(n>=1200){
+  console.log('“У вас высокий уровень дохода”');
+}else if(1200>n>600){
+  console.log('“У вас средний уровень дохода”')
+}else if(600>n>0){
+  console.log('“К сожалению у вас уровень дохода ниже среднего”')
+}else{
+  console.log('“Что то пошло не так”');
+}
+
+
+
+
+
+
+
+
+
