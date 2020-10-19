9822211dab120a84c7fb5052af2f95e4a2f12095
Pavel Yakovlev <pavel.yakovlev.1984@bk.ru>
2020-10-19 20:31:17 +0300

    lesson_04

---
+++ lesson04/js/script.js
@@ -0,0 +1,91 @@
+'use strict';
+let mission = 600000;
+  console.log(money);
+  console.log(income); 
+  console.log(deposit); 
+
+  console.log(typeof money);
+  console.log(typeof income);
+  console.log(typeof deposit);
+
+
+  console.log(addExpenses.length);
+
+
+  console.log('"Период равен'+' '+ period +' '+'месяцев” и “Цель заработать' +' '+ mission + ' '+'рублей"');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(' '));
+
+
+
+console.log(budgetDay);
+
+
+ money = prompt('“Ваш месячный доход?”');
+
+
+ addExpenses = prompt('“Перечислите возможные расходы за рассчитываемый период через запятую”');
+
+ deposit = Boolean(prompt('“Есть ли у вас депозит в банке?”'));
+
+ expenses1 = prompt('“Введите обязательную статью расходов?”');
+ expenses2 = prompt('“Введите обязательную статью расходов?”');
+ amount1 = prompt('“Во сколько это обойдется?”');
+ damount2 = prompt('“Во сколько это обойдется?”');
+
+
+ 
+ 
+ let budgetMonth = money - addExpenses;
+
+console.log('Бюджет на месяц'+ budgetMonth);
+
+let missionPeriod = mission / budgetMonth;
+
+console.log('Бюджет на месяц'+ missionPeriod);
+
+
+let MPeriod = Math.ceil(missionPeriod);
+
+console.log('Цель будет достигнута за:'+ missionPeriod + 'месяца');
+
+
+
+
+
+console.log('Бюджет на день:'+ budgetDay);
+
+
+
+if(budgetDay>=1200){
+  console.log('“У вас высокий уровень дохода”');
+}else if(1200>budgetDay>600){
+  console.log('“У вас средний уровень дохода”')
+}else if(600>budgetDay>0){
+  console.log('“К сожалению у вас уровень дохода ниже среднего”')
+}else{
+  console.log('“Что то пошло не так”');
+}
+
+function getExpensesMonth(amount1, damount2){
+  return amount1 + damount2;
+  
+};
+
+function getAccumulatedMonth(){
+return money - addExpenses;
+};
+
+const accumulatedMonth =  getAccumulatedMonth();
+
+
+function getTargetMonth(){
+  return  mission / accumulatedMonth;
+};
+
+function budgetDay(accumulatedMonth){
+return accumulatedMonth();
+};
+
+getStatusIncome();
